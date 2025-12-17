9394afe9ef725bb466d497a9feb0f9d138f25269
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2025-12-17 13:41:43 +0300

    finished sql homework , its legit the same from last year

---
+++ carmen-hints.sql
@@ -0,0 +1,109 @@
+-- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
+-- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
+-- so find the least populated country in Southern Europe, and we'll start looking for her there.
+
+
+
+carmen=# select name,population from country where region like 'Southern Europe' order by  population asc limit 1;
+;
+             name              | population 
+-------------------------------+------------
+ Holy See (Vatican City State) |       1000
+(1 row)
+
+
+
+-- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
+-- this country's officially recognized language. Check our databases and find out what language is
+-- spoken in this country, so we can call in a translator to work with you.
+
+carmen=# select * from countrylanguage where countrycode like 'VAT'
+carmen-# ;
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ VAT         | Italian  | t          |          0
+(1 row)
+
+carmen=# 
+
+-- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
+-- to a different country, a country where people speak only the language she was learning. Find out which
+--  nearby country speaks nothing but that language.
+
+carmen=# select * from countrylanguage where language like 'Italian' and percentage = 100;
+;
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ SMR         | Italian  | t          |        100
+(1 row)
+
+-- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
+ -- There are only two cities she could be flying to in the country. One is named the same as the country – that
+ -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
+ --  be flying to.
+
+carmen=# select * from city where countrycode like 'SMR'
+carmen-# ;
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+ 3171 | San Marino | SMR         | San Marino        |       2294
+(2 rows)
+
+carmen=# 
+
+
+-- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
+-- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
+-- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+
+carmen=# select * from city where name like 'Serr%';
+;
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+  265 | Serra      | BRA         | Espï¿½rito Santo  |     302666
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+(2 rows)
+
+
+-- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
+ -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
+ -- follow right behind you!
+
+carmen=# select capital from country where code like 'BRA';
+ capital 
+---------
+     211
+(1 row)
+
+carmen=# select name from city where id = 211;
+    name    
+------------
+ Brasï¿½lia --why is it this way ????
+(1 row)
+
+carmen=# 
+
+-- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
+ -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
+ -- landing dock. Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+-- if we can crack it, we can finally put her where she belongs – behind bars.
+
+-- Our playdate of late has been unusually fun –
+-- As an agent, I'll say, you've been a joy to outrun.
+-- And while the food here is great, and the people – so nice!
+-- I need a little more sunshine with my slice of life.
+-- So I'm off to add one to the population I find
+-- In a city of ninety-one thousand and now, eighty five.
+
+
+-- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+
+carmen=# select * from city where population =91084
+carmen-# ;
+  id  |     name     | countrycode |  district  | population 
+------+--------------+-------------+------------+------------
+ 4060 | Santa Monica | USA         | California |      91084
+(1 row)
+-- She's in ____________________________!
